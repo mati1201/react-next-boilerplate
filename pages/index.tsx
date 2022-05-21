0ecc9d121de24b452b45f27f9bc5d46ccf1d5ec0
unknown <mati.kaczmarek@op.pl>
2022-05-21 17:47:57 +0200

    Installed React 18, Next 12 and SASS Modules

---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Image from 'next/image'
+
+import logo from '../public/images/logo.svg';
+import styles from '../src/containers/Home/Home.module.scss';
+
+const Home = () => (
+  <div className={styles.app}>
+    <header className={styles.appHeader}>
+      <Image
+        src={logo}
+        alt="React Logo"
+        className={styles.appLogo}
+        width={200}
+        height={200}
+      />
+      <a
+        className={styles.appLink}
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
+
+export default Home;
